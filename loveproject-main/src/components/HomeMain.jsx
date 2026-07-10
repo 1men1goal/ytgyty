becb15996b25e73e95a067d9ddd0630ae41a6ea4
1men1goal <06samiking06@gmail.com>
2026-07-10 18:33:50 +0600

    Add files via upload

---
+++ loveproject-main/src/components/HomeMain.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LovePurpose from './LovePurpose'
+import LovePurposeHindi from './LovePurposeHindi'
+import FlirtyLoveProposal from './FlirtyLoveProposal'
+
+const HomeMain = () => {
+  return (
+    <div>
+        {/* <LovePurpose/> */}
+        {/* <LovePurposeHindi/> */}
+        <FlirtyLoveProposal/>
+    </div>
+  )
+}
+
+export default HomeMain
